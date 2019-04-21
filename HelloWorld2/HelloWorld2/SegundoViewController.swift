9233f03db0c9c98fada7067295badfc74d4c224c
netfanely <fanely@gmail.com>
2019-04-21 00:03:58 -0500

    segunda view

---
+++ HelloWorld2/HelloWorld2/SegundoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SegundoViewController.swift
+//  HelloWorld2
+//
+//  Created by Fanely on 4/20/19.
+//  Copyright © 2019 Net Fanely. All rights reserved.
+//
+
+import UIKit
+
+class SegundoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HelloWorld2/HelloWorld2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Fanely on 4/20/19.
+//  Copyright © 2019 Net Fanely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
